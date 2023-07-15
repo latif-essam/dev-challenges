6b8e9cc64e4c1c2c3546bcf7ba1b4fbef17feac2
Latif Essam <latifekhaliel@gmail.com>
2023-07-15 12:57:03 +0300

    fixing confliects

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-<<<<<<< HEAD
-import "./App.scss";
-import NotFound from "./Paths/Responsive-web/404-Not-Found";
-=======
 import "./App.css";
 import MyTeamPage from "./Paths/Responsive-web/My-Team-Page";
 
->>>>>>> 951dcbb506dbb9b68c96e3af7e82cbc3b523b1e5
 function App() {
   return (
     <div className="app">
-<<<<<<< HEAD
-      <NotFound />
-=======
       <MyTeamPage />
->>>>>>> 951dcbb506dbb9b68c96e3af7e82cbc3b523b1e5
     </div>
   );
 }
